396eec6a01f161efe41ea585533b06567a19197b
dizhongdi <2755063993@qq.com>
2022-09-15 23:59:39 +0800

    扣减下载次数

--- service/service-user/src/main/java/com/dizhongdi/serviceuser/controller/api/ApiCreditController.java
+++ service/service-user/src/main/java/com/dizhongdi/serviceuser/controller/api/ApiCreditController.java
@@ -1,13 +1,11 @@
 package com.dizhongdi.serviceuser.controller.api;
 
 
+import com.dizhongdi.serviceuser.entity.DzdCredit;
 import com.dizhongdi.serviceuser.service.DzdCreditService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -28,6 +26,17 @@ public class ApiCreditController {
     @ApiOperation(value = "根据用户id获取剩余下载次数")
     public Integer getQuantityById(@PathVariable String memberId){
         return creditService.getById(memberId).getQuantity();
+    }
+
+    @PutMapping("cutQuantityById/{memberId}/{num}")
+    @ApiOperation(value = "减少下载次数")
+    public boolean cutQuantityById(@PathVariable String memberId , @PathVariable Integer num){
+        DzdCredit credit = creditService.getById(memberId);
+        if (credit.getCredit()>0){
+            creditService.updateById(credit.setQuantity(credit.getQuantity() - num));
+            return true;
+        }
+        return false;
     }
 
 }
